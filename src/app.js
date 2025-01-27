a97ebf5e896ff97051470d0b8e4482ce28caec34
Kacian417 <knicho4492@gmail.com>
2025-01-27 19:35:18 +0000

    complete pre-work assignment

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+  let noun = ["The dog ", "The man ", "The woman ", "The father "];
+  let verb = ["ate ", "caught ", "found ", "stole "];
+  let object = ["the ball ", "a sandwich ", "a cat ", "a lightsaber"];
+  let time = ["this afternoon.", "yesterday.", "this morning.", "just now."];
+
+  let random1 = Math.floor(Math.random() * noun.length);
+  let random2 = Math.floor(Math.random() * verb.length);
+  let random3 = Math.floor(Math.random() * object.length);
+  let random4 = Math.floor(Math.random() * time.length);
+
+  document.querySelector(".excuse").innerHTML =
+    noun[random1] + verb[random2] + object[random3] + time[random4];
+
   console.log("Hello Rigo from the console!");
 };
